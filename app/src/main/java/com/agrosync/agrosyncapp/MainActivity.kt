6f68a0cef4002499dff7a0ad0d600b5754971764
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-11-19 09:14:11 -0300

    19/11/2024 - Feat - Login Auth

--- app/src/main/java/com/agrosync/agrosyncapp/MainActivity.kt
+++ app/src/main/java/com/agrosync/agrosyncapp/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.agrosync.agrosyncapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.agrosync.agrosyncapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            val displayName =currentUser.displayName
+            val userName = if(displayName.isNullOrEmpty()){
+                currentUser.email?: "Usuario sem nome"
+            }else {
+                displayName + "\nSeja bem-vindo"
+            }
+            binding.tvUsuario.text = userName
+        }else {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.deslogar.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
