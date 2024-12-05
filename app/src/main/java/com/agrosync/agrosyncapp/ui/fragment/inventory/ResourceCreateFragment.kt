9cbe7694a72761cacdb9cbb38e7981c7dd6bce3c
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-12-05 16:56:26 -0300

    05/12/2024 - Fix - Resource Create Screen

--- app/src/main/java/com/agrosync/agrosyncapp/ui/fragment/inventory/ResourceCreateFragment.kt
+++ app/src/main/java/com/agrosync/agrosyncapp/ui/fragment/inventory/ResourceCreateFragment.kt
@@ -1,24 +1,31 @@
 package com.agrosync.agrosyncapp.ui.fragment.inventory
 
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.agrosync.agrosyncapp.R
-import com.agrosync.agrosyncapp.data.model.ResourceCategory
+import com.agrosync.agrosyncapp.data.model.MeasureUnit
 import com.agrosync.agrosyncapp.databinding.FragmentResourceCreateBinding
 
 class ResourceCreateFragment : Fragment() {
     private var _binding: FragmentResourceCreateBinding? = null
     private val binding get() = _binding!!
-    private lateinit var spinner: Spinner
+    private lateinit var spinnerCategories: Spinner
+    private lateinit var spinnerMeasureUnit: Spinner
     private lateinit var spinnerAdapter: ArrayAdapter<String>
+    private lateinit var spinnerMeasureUnitAdapter: ArrayAdapter<String>
+    private lateinit var resourceImage: ImageView
+    private lateinit var btnSelectImage: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,19 +38,21 @@ class ResourceCreateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Inicializando o Spinner
-        spinner = view.findViewById(R.id.spinnerCategories)
 
-        // Dados para o Spinner
+        spinnerCategories = view.findViewById(R.id.spinnerCategories)
+        spinnerMeasureUnit = view.findViewById(R.id.spinnerMeasureUnit)
+        resourceImage = binding.resourceImage
+        btnSelectImage = binding.btnSelectImage
+
+        // Inicializando o Spinner Categories
         val items = listOf("Item 1", "Item 2", "Item 3")
 
-        // Configurando o Adapter
+
         spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, items)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = spinnerAdapter
+        spinnerCategories.adapter = spinnerAdapter
 
-        // Configurar o Listener para mudanças
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        spinnerCategories.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parentView: AdapterView<*>, view: View?, position: Int, id: Long) {
                 // Lógica quando um item é selecionado
                 Toast.makeText(requireContext(), "Selecionado: ${items[position]}", Toast.LENGTH_SHORT).show()
@@ -52,6 +61,34 @@ class ResourceCreateFragment : Fragment() {
             override fun onNothingSelected(parentView: AdapterView<*>) {}
         }
 
+        // Dados para o Spinner Unidades de de Medida
+
+        val mUnits = MeasureUnit.entries.map { it.displayName }
+
+        spinnerMeasureUnitAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, mUnits)
+        spinnerMeasureUnitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerMeasureUnit.adapter = spinnerMeasureUnitAdapter
+
+        spinnerMeasureUnit.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedUnit = MeasureUnit.entries[position]
+                Toast.makeText(requireContext(), "Selecionado: $selectedUnit", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>) {}
+        }
+
+        //Image Upload
+        btnSelectImage.setOnClickListener {
+            // Abrir o seletor de imagens
+            selectImageLauncher.launch("image/*")
+        }
+    }
+    private val selectImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let {
+            // Definir a imagem selecionada no ImageView
+            resourceImage.setImageURI(it)
+        }
     }
 
     override fun onDestroyView() {
